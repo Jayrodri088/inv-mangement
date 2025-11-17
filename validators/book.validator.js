89de8fb0599ce624187412e79a085bdf4a921124
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-11-17 18:10:00 +0100

    let's go

--- validators/book.validator.js
+++ validators/book.validator.js
@@ -36,7 +36,7 @@ const updateBookValidator = [
   body("image").optional().isURL().withMessage("Image must be a valid URL"),
 ];
 
-const handleValidationErrors = () => {
+const handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
