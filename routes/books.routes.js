7aba2c54588aaada43c9fb20869c8434d7b023f7
Jayrodri088 <johnbetho.c.088@gmail.com>
2025-11-17 18:03:06 +0100

    express-validator

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const BookModel = require("../models/book.model");
 
+const { body, validationResult, param } = require("express-validator");
+
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const newBook = await BookModel.create(req.body);
-    res.status(201).json(newBook);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+router.post(
+  "/",
+  [
+    body("bookName")
+      .notEmpty()
+      .withMessage("Book name is required")
+      .isLength({ min: 6, max: 100 })
+      .withMessage("Book name must be between 6 and 100 characters"),
+    body("countInStock")
+      .notEmpty()
+      .withMessage("Count in stock is required")
+      .isInt({ min: 0, max: 1000 })
+      .withMessage("Count in stock must be between 0 and 1000"),
+    body("price")
+      .notEmpty()
+      .withMessage("Price is required")
+      .isFloat({ min: 0, max: 10000 })
+      .withMessage("Price must be between 0 and 10000$"),
+    body("image").optional().isURL().withMessage("Image must be a valid URL"),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const newBook = await BookModel.create(req.body);
+      res.status(201).json(newBook);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-});
+);
 
 router.get("/", async (req, res) => {
   try {
@@ -51,23 +78,47 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
+router.put(
+  "/:id",
+  [
+    param("id").isMongoId().withMessage("Invalid book ID"),
+    body("bookName")
+      .optional()
+      .isLength({ min: 6, max: 100 })
+      .withMessage("Book name must be between 6 and 100 characters"),
+    body("countInStock")
+      .optional()
+      .isInt({ min: 0, max: 1000 })
+      .withMessage("Count in stock must be between 0 and 1000"),
+    body("price")
+      .optional()
+      .isFloat({ min: 0, max: 10000 })
+      .withMessage("Price must be between 0 and 10000$"),
+    body("image").optional().isURL().withMessage("Image must be a valid URL"),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    if (!updatedBook) {
-      return res.status(404).json({ message: "Book not found" });
+      const { id } = req.params;
+      const updatedBook = await BookModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+
+      if (!updatedBook) {
+        return res.status(404).json({ message: "Book not found" });
+      }
+
+      res
+        .status(200)
+        .json({ message: "Book updated successfully", book: updatedBook });
+    } catch (error) {
+      res.status(400).json({ message: error.message });
     }
-
-    res
-      .status(200)
-      .json({ message: "Book updated successfully", book: updatedBook });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
   }
-});
+);
 
 module.exports = router;
